8838cb08343fab36249cd31ce73b5f5f96303744
WanBenQAQ <52995164+WanBenQAQ@users.noreply.github.com>
2021-10-13 20:31:20 +0800

    Delete resp directory

--- resp/Entity.go
+++
@@ -1,9 +0,0 @@
-package resp
-
-type Entity struct {
-	Code      int         `json:"code"`
-	Msg       string      `json:"msg"`
-	Total     int         `json:"total"`
-	TotalPage int         `json:"totalPage"`
-	Data      interface{} `json:"data"`
-}
